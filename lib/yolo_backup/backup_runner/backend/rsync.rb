09e3037abb8aed205f0f1f8773f060884e4a3c95
Nils Caspar <ncaspar@me.com>
2013-05-26 21:55:22 +0200

    Refactor options

--- lib/yolo_backup/backup_runner/backend/rsync.rb
+++ lib/yolo_backup/backup_runner/backend/rsync.rb
@@ -5,7 +5,7 @@ class YOLOBackup::BackupRunner::Backend::Rsync < YOLOBackup::BackupRunner::Backe
 
   DEFAULT_RSYNC_OPTIONS = '-aAX --numeric-ids'
 
-  attr_reader :server
+  attr_reader :server, :path
 
   def initialize(server)
     @server = server
@@ -13,19 +13,29 @@ class YOLOBackup::BackupRunner::Backend::Rsync < YOLOBackup::BackupRunner::Backe
 
   def start_backup
     server.storage.initiate_backup(server) do |path|
-      options = [DEFAULT_RSYNC_OPTIONS]
-      if server.latest_backup
-        latest_backup = File.expand_path("#{path}/../#{server.latest_backup}")
-        options << "--link-dest='#{latest_backup}'"
-      end
-      ssh_options = []
-      ssh_options << "-i '#{server.ssh_key}'" unless server.ssh_key.nil?
-      ssh_options << "-p '#{server.ssh_port}'" unless server.ssh_port.nil?
-      options << "-e \"ssh #{ssh_options.join(' ')}\""
-      options << "--exclude={#{server.excludes.join(',')}}"
+      @path = path
       command = "rsync #{options.join(' ')} #{server.ssh_user || 'root'}@#{server.ssh_host || server}:/ '#{path}'"
       `#{command}`
       raise RsyncFailedError, "rsync command failed: #{$?}" unless $?.success?
     end
+  end
+
+  private
+  def options
+    options = [DEFAULT_RSYNC_OPTIONS]
+    if server.latest_backup
+      latest_backup = File.expand_path("#{path}/../#{server.latest_backup}")
+      options << "--link-dest='#{latest_backup}'"
+    end
+    options << "-e \"ssh #{ssh_options.join(' ')}\""
+    options << "--exclude={#{server.excludes.join(',')}}"
+    options
+  end
+
+  def ssh_options
+    ssh_options = []
+    ssh_options << "-i '#{server.ssh_key}'" unless server.ssh_key.nil?
+    ssh_options << "-p '#{server.ssh_port}'" unless server.ssh_port.nil?
+    ssh_options
   end
 end
