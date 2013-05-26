ad372d7e0f7aaef6e7d0aaa34357a38c866ae7ba
Nils Caspar <ncaspar@me.com>
2013-05-26 13:03:42 +0200

    Config example

--- lib/yolo_backup/cli.rb
+++ lib/yolo_backup/cli.rb
@@ -2,7 +2,10 @@ require 'thor'
 
 module YOLOBackup
   class CLI < Thor
+    DEFAULT_CONFIG_FILE = '/etc/yolo_backup.yml'
+
     class_option :verbose, :type => :boolean
+    class_option :config, :type => :string, :banner => "path to config file (default #{DEFAULT_CONFIG_FILE})"
 
     desc 'status [SERVER]', 'Display backup status and statistics'
     def status(server = nil)
@@ -12,6 +15,11 @@ module YOLOBackup
     desc 'backup [SERVER]', 'Start backup process'
     def backup(server = nil)
       puts 'BACKUP'
+    end
+
+    private
+    def config
+      @config ||= YAML::load(File.open(options[:config] || DEFAULT_CONFIG_FILE))
     end
   end
 end
